f1fe50804bb0de06d3825406ab4a3cec2d5612da
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-08-09 12:58:31 -0500

    Updating Code
    Implementing calculate.js and operate.js to my project.
    
    Running linters

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,50 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import Button from './Button';
 import calculate from '../logic/calculate';
 
 function Calculator() {
-  const [calcState, setCalcState] = React.useState({ total: null, next: null, operation: null });
+  const [calculatorData, setCalculatorData] = useState({});
+  const { total, next } = calculatorData;
+  const result = next || total || '0';
 
-  const handleButtonClick = (buttonName) => {
-    const newCalcState = calculate(calcState, buttonName);
-    setCalcState(newCalcState);
-  };
+  function handleClick(buttonName) {
+    const newCalculatorData = calculate(calculatorData, buttonName);
+    setCalculatorData(newCalculatorData);
+  }
 
   return (
     <div className="calculator">
       <div className="screen">
-        {calcState.next || calcState.total || '0'}
+        {result}
       </div>
       <div className="buttons">
         <div className="row">
-          <Button label="AC" onClick={() => handleButtonClick('AC')} />
-          <Button label="+/-" onClick={() => handleButtonClick('+/-')} />
-          <Button label="%" onClick={() => handleButtonClick('%')} />
-          <Button label="÷" special onClick={() => handleButtonClick('÷')} />
+          <Button label="AC" onClick={() => handleClick('AC')} />
+          <Button label="+/-" onClick={() => handleClick('+/-')} />
+          <Button label="%" onClick={() => handleClick('%')} />
+          <Button label="÷" special onClick={() => handleClick('÷')} />
         </div>
         <div className="row">
-          <Button label="7" onClick={() => handleButtonClick('7')} />
-          <Button label="8" onClick={() => handleButtonClick('8')} />
-          <Button label="9" onClick={() => handleButtonClick('9')} />
-          <Button label="*" special onClick={() => handleButtonClick('*')} />
+          <Button label="7" onClick={() => handleClick('7')} />
+          <Button label="8" onClick={() => handleClick('8')} />
+          <Button label="9" onClick={() => handleClick('9')} />
+          <Button label="*" special onClick={() => handleClick('x')} />
         </div>
         <div className="row">
-          <Button label="4" onClick={() => handleButtonClick('4')} />
-          <Button label="5" onClick={() => handleButtonClick('5')} />
-          <Button label="6" onClick={() => handleButtonClick('6')} />
-          <Button label="-" special onClick={() => handleButtonClick('-')} />
+          <Button label="4" onClick={() => handleClick('4')} />
+          <Button label="5" onClick={() => handleClick('5')} />
+          <Button label="6" onClick={() => handleClick('6')} />
+          <Button label="-" special onClick={() => handleClick('-')} />
         </div>
         <div className="row">
-          <Button label="1" onClick={() => handleButtonClick('1')} />
-          <Button label="2" onClick={() => handleButtonClick('2')} />
-          <Button label="3" onClick={() => handleButtonClick('3')} />
-          <Button label="+" special onClick={() => handleButtonClick('+')} />
+          <Button label="1" onClick={() => handleClick('1')} />
+          <Button label="2" onClick={() => handleClick('2')} />
+          <Button label="3" onClick={() => handleClick('3')} />
+          <Button label="+" special onClick={() => handleClick('+')} />
         </div>
         <div className="row">
-          <Button label="0" doubleWidth onClick={() => handleButtonClick('0')} />
-          <Button label="." onClick={() => handleButtonClick('.')} />
-          <Button label="=" special onClick={() => handleButtonClick('=')} />
+          <Button label="0" doubleWidth onClick={() => handleClick('0')} />
+          <Button label="." onClick={() => handleClick('.')} />
+          <Button label="=" special onClick={() => handleClick('=')} />
         </div>
       </div>
     </div>
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,26 +1,30 @@
 import Big from 'big.js';
 
-function operate(numberOne, numberTwo, operation) {
-  console.log('operate:', numberOne, numberTwo, operation);
-  const one = Big(numberOne || '0');
-  const two = Big(numberTwo || '0');
-
+export default function operate(numberOne, numberTwo, operation) {
+  const one = Big(numberOne);
+  const two = Big(numberTwo);
   if (operation === '+') {
     return one.plus(two).toString();
   }
   if (operation === '-') {
     return one.minus(two).toString();
   }
-  if (operation === '*') {
+  if (operation === 'x') {
     return one.times(two).toString();
   }
   if (operation === '÷') {
-    if (two.eq('0')) {
-      return 'NaN';
+    try {
+      return one.div(two).toString();
+    } catch (err) {
+      return "Can't divide by 0.";
     }
-    return one.div(two).toString();
+  }
+  if (operation === '%') {
+    try {
+      return one.mod(two).toString();
+    } catch (err) {
+      return "Can't find modulo as can't divide by 0.";
+    }
   }
   throw Error(`Unknown operation '${operation}'`);
 }
-
-export default operate;
