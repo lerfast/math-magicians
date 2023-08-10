03656b8767988b9e4c7d6b958c292652d1058556
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-08-10 11:09:59 -0500

    Updating code for  Fetch api to Quotes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Calculator from './Components/Calculator';
+import QuoteDisplay from './Components/QuoteDisplay';
 
 function App() {
   return (
     <div className="app">
       <h1>Math Magicians Calculator by Luis Emilio Rojas</h1>
       <Calculator />
+      <QuoteDisplay />
+      {' '}
+      <QuoteDisplay />
     </div>
   );
 }
